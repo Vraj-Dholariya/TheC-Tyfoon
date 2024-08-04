16f97daf6f77e907a5d666658e4770999478e2f7
Vraj Dholariya <vrajdholariya3@gmail.com>
2024-08-04 18:28:19 +0530

    04/08/2024

---
+++ PR-2/pr_1.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Train {
+private:
+    int trainNumber;
+    char trainName[50];
+    char source[50];
+    char destination[50];
+    char trainTime[10];
+    static int trainCount;
+
+public:
+    Train() {
+        trainNumber = 0;
+        strcpy(trainName, "");
+        strcpy(source, "");
+        strcpy(destination, "");
+        strcpy(trainTime, "");
+        trainCount++;
+    }
+
+    Train(int trainNumber, char trainName[], char source[], char destination[], char trainTime[]) {
+        this->trainNumber = trainNumber;
+        strcpy(this->trainName, trainName);
+        strcpy(this->source, source);
+        strcpy(this->destination, destination);
+        strcpy(this->trainTime, trainTime);
+        trainCount++;
+    }
+
+    ~Train() {
+        trainCount--;
+    }
+
+    void inputTrainDetails() {
+        cout << "Enter Train Number: ";
+        cin >> trainNumber;
+
+        cout << "Enter Train Name: ";
+        cin >> trainName;
+
+        cout << "Enter Source: ";
+        cin >> source;
+
+        cout << "Enter Destination: ";
+        cin >> destination;
+
+        cout << "Enter Train Time: ";
+        cin >> trainTime;
+    }
+
+    void displayTrainDetails() const {
+        cout << endl;
+        cout << "Train Number: " << trainNumber << endl;
+        cout << "Train Name: " << trainName << endl;
+        cout << "Source: " << source << endl;
+        cout << "Destination: " << destination << endl;
+        cout << "Train Time: " << trainTime << endl;
+        cout << "---------------------------" << endl;
+    }
+
+    int getTrainNumber()const{
+        return trainNumber;
+    }
+
+    static int getTrainCount() {
+        return trainCount;
+    }
+};
+
+int Train::trainCount = 0;
+
+class RailwayResevationSystem {
+private:
+    Train trains[10];
+    int totalTrains;
+
+public:
+    RailwayResevationSystem() : totalTrains(0) {}
+
+    void addNewTrain() {
+        if (totalTrains < 10) {
+            trains[totalTrains].inputTrainDetails();
+            totalTrains++;
+        } else {
+            cout << "Maximum train limit reached!" << endl;
+        }
+    }
+
+    void displayAllTrains() const {
+        if (totalTrains == 0) {
+            cout << "No trains to display!" << endl;
+        } else {
+            for (int i = 0; i < totalTrains; i++) {
+                cout << "Train " << i + 1 << " details:" << endl;
+                trains[i].displayTrainDetails();
+            }
+        }
+    }
+
+    void searchTrainByNumber(int number) const {
+        bool found = false;
+        for (int i = 0; i < totalTrains; i++) {
+            if (trains[i].getTrainNumber() == number) {
+                cout << "Train details:" << endl;
+                trains[i].displayTrainDetails();
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            cout << "Train with number " << number << " not found!" << endl;
+        }
+    }
+};
+
---
+++ PR-2/pr_1_main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string.h>
+#include "pr_1.cpp"
+using namespace std;
+
+int main()
+{   
+    RailwayResevationSystem system;
+    int choice;
+    int number;
+     do
+     {
+        cout << "---Railway Resevation System---"<<endl 
+        << "1.Add New Train" << endl
+        << "2.Display All Train Records" <<endl
+        << "3.Search Train By Train Number" << endl
+        << "4.Exit" << endl;
+
+        cout << "Enter Your Choice: ";
+        cin >> choice;     
+        
+        switch (choice)
+        {
+            case 1:
+                system.addNewTrain();
+                break;
+            case 2:
+                system.displayAllTrains();
+                break;
+            case 3:
+                system.searchTrainByNumber(number);
+                break;
+            case 4:
+                cout << "Exiting the system. Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }        
+     }while ( choice != 4);
+ return 0;    
+}
